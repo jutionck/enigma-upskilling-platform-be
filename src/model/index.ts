555b9929fddf66da6f99684440ed2275ac20f0bb
jutionck <jutionck@gmail.com>
2025-03-14 10:19:06 +0700

    still failed connect to firestore

--- src/model/index.ts
+++
@@ -1,3 +0,0 @@
-import { User } from "./entity/user.model";
-
-export { User };
--- src/routes/index.ts
+++
@@ -1,4 +0,0 @@
-import { Elysia } from "elysia";
-import {userRoutes} from "@/routes/user.route";
-
-export const routes = new Elysia().use(userRoutes);
